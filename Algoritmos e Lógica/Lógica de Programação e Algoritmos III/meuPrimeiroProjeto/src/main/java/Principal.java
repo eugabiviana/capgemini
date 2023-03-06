427d039f09c031882aca342ed639c3e997912b1b
eugabiviana <gabi.viana88@gmail.com>
2023-03-06 20:24:47 -0300

    OPrimeiros passos - criando um projeto

---
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/meuPrimeiroProjeto/src/main/java/Principal.java
@@ -0,0 +1,6 @@
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Again...");
+        System.out.println("Meu segundo comando de saída.");
+    }
+}
