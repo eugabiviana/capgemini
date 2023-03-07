dc9e3e92b0b6b48f53ee6de9822cbb4eaaff502b
eugabiviana <gabi.viana88@gmail.com>
2023-03-07 00:50:05 -0300

    Aula 4.4 - tipos de comentários e suas funÃcoes dentro do código

--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/meuPrimeiroProjeto/src/main/java/Principal.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/meuPrimeiroProjeto/src/main/java/Principal.java
@@ -2,5 +2,14 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Hello, world! Again...");
         System.out.println("Meu segundo comando de saída.");
+
+        // Comentário de linha
+        /* Comentário em bloco.
+        * posso pular linha de buenas. Serve para uma informação maior dentro do código*/
+        /**
+         * Esse é o modelo de comentário de documentação. Posso usá-lo para gerar um documento de documentação ao
+         * gerar o meu algoritmo em outro app.
+         * É parecido com o de bloco, mas o de bloco não me dá essa opção.
+         */
     }
 }
