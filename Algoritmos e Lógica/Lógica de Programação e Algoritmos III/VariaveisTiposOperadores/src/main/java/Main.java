cddd1cecb9e9694022b3810d7383c6cdf3983325
eugabiviana <gabi.viana88@gmail.com>
2023-03-07 01:45:44 -0300

    OAuÃla 4.6 - tipos de variaveis e operadores aritmeticos

---
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/VariaveisTiposOperadores/src/main/java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Tipos de variáveis:
+        //Variáveis primitivas:
+        int idade = 0; // <- posso iniciar a variável ou não;
+        float cotacaoDolar = 5.0f; //armazena números reais, ou seja, com vírgula. Devo colocar f ao final;
+        double cotacaoEuro = 6.0d; //idem float, mas armazena números maiores. idem float: d no final;
+        char genero = 'M'; //armazena só uma letra, escreve com aspas simples;
+        byte pontos = 0; //também armazena números inteiros;
+        boolean estaCadastrado = false; //armazena valores lógicos (true ou false).
+
+        //Não primitivos:
+        String nome = "Esse é um texto de uma variável String!"; //escreve com aspas duplas;
+
+        /**
+         * Operadores aritméticos:
+         * +
+         * -
+         * /
+         * *
+         * %
+         */
+
+
+
+
+    }
+}
