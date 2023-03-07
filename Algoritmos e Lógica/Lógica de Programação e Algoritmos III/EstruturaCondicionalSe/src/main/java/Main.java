d6a47af43da8bc8c995cb27bd5d61a78b78aa104
eugabiviana <gabi.viana88@gmail.com>
2023-03-07 02:02:23 -0300

    OAula 4.8 - If, operadores relacionais e lógicos

---
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/EstruturaCondicionalSe/src/main/java/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //If
+
+        int media = 7;
+        if (media >= 7){
+            if (media == 10){
+                System.out.println("Parabéns! Você passou com excelência. <3");
+            } else {
+                System.out.println("Aprovado!");
+            }
+        }else {
+            System.out.println("Reprovado!");
+        }
+
+        /**
+         * Operadores relacionais:
+         *  >
+         *  >=
+         *  <
+         *  <=
+         *  == (comparando com um valor. O simples é atribuição de valor, o <- do portugol.)
+         *  != (diferente de)         *
+         */
+
+        /**
+         * Operadores lógicos:
+         *  &&
+         *  ||
+         *  ! (negação)         *
+         */
+
+    }
+}
