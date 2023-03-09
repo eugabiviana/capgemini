218629fcb84c3370745464609707c88017f21e76
eugabiviana <gabi.viana88@gmail.com>
2023-03-09 10:46:55 -0300

    OAula 4.89- IEscolha caso - switch

---
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/EstruturaEscolha/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int codigoProduto = 2;
+
+        switch (codigoProduto){
+
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            default: //trata caso que seja diferente das opções listadas.
+        }
+    }
+}
