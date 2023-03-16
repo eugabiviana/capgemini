47820bad996a7a2620ec699b3409e0c97170ee26
eugabiviana <gabi.viana88@gmail.com>
2023-03-16 15:46:40 -0300

    Aula 4.134- exercício 22 - corrigindo e finalizando a atividade

--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/ListaDeExercicios/src/main/java/Exercicio22.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/ListaDeExercicios/src/main/java/Exercicio22.java
@@ -16,18 +16,19 @@ public class Exercicio22 {
         float precoCusto;
         float precoVenda;
 
-        float totalCusto;
-        float totalVenda;
+        float totalCusto = 0.0f; //se eu não iniciar a variável aqui, terei um erro na conta dentro do for*
+        float totalVenda = 0.0f; //e se não colocar o f, dá erro na execução!
 
-        for (int i = 0; i < 40; i++){
+        int i = 0; //criando o índice aqui fora para não dar erro fora do laço!*
+        for (; i < 3; i++){
             System.out.println("Digite o nome do produto: ");
-            nomeProduto = leia.nextLine();
+            nomeProduto = leia.next();
             System.out.println("Digite o preço de custo do produto: ");
             precoCusto = leia.nextFloat();
             System.out.println("Digite o preço de venda do produto: ");
             precoVenda = leia.nextFloat();
 
-            totalCusto = totalCusto + precoCusto;
+            totalCusto = totalCusto + precoCusto; //*dá erro aqui e nessa linha de baixo!
             totalVenda = totalVenda + precoVenda;
 
             if (precoCusto == precoVenda){
@@ -40,6 +41,11 @@ public class Exercicio22 {
                     System.out.println("Houve lucro!");
                 }
             }
+
+            System.out.println(nomeProduto + ": preço de custo = " + precoCusto + ", preço de venda = " + precoVenda);
         }
+
+        System.out.println("A média do preço de custo é de: " + (totalCusto/i)); //*Daria erro aqui!
+        System.out.println("A média do preço de venda é de: " + (totalVenda/i));
     }
 }
