31d870a326acc3b257c991a19498bacaec49a0c1
eugabiviana <gabi.viana88@gmail.com>
2023-03-17 13:16:29 -0300

    Aula 4.156- introducao de POO - criando mÃetodo

--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Pessoa.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Pessoa.java
@@ -6,7 +6,11 @@ public class Pessoa {
     float peso;
     float altura;
 
-    //métodos
+    //métodos (sempre colocar o nome dos métodos no infinitivo)
+    public float calcularIMC(){
+        float imc = peso / (altura * altura);
+        return imc;
+    }
 
 
 }
