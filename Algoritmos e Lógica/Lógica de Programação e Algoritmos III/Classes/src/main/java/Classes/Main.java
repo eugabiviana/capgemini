4d3fa5e36845ca54da4f62fdf6a67def870296dd
eugabiviana <gabi.viana88@gmail.com>
2023-03-17 15:58:16 -0300

    Aula 4.17/4.T - POO - criando um objeto

--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Main.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Main.java
@@ -1,7 +1,20 @@
 package Classes;
+import Classes.Pessoa;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        //criando uma variável não-primitiva:
+        Pessoa objetoPessoa = new Pessoa(); //guardo na VARIÁVEL objetoPessoas as infos de Pessoa
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite o peso da pessoa:");
+        objetoPessoa.peso = leia.nextFloat();
+
+        System.out.println("Digite a altura da pessoa:");
+        objetoPessoa.altura = leia.nextFloat();
+
+        System.out.println("IMC = " + objetoPessoa.calcularIMC());
     }
 }
--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Pessoa.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Classes/src/main/java/Classes/Pessoa.java
@@ -3,14 +3,12 @@ package Classes;
 public class Pessoa {
 
     //atributos
-    float peso;
-    float altura;
+    public float peso;
+    public float altura;
 
     //métodos (sempre colocar o nome dos métodos no infinitivo)
     public float calcularIMC(){
         float imc = peso / (altura * altura);
         return imc;
     }
-
-
 }
