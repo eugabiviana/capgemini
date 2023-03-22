bc99c40b02fb883b2b15e19913623724fa791a06
eugabiviana <gabi.viana88@gmail.com>
2023-03-22 14:15:25 -0300

    Aula 4.21/4X - criando e explicando heranca

--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Heranca/src/main/java/Heranca/Main.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Heranca/src/main/java/Heranca/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         Vendedor v = new Vendedor();
+
         Motorista m = new Motorista();
     }
 }
+
+/**
+ * A herança é usada para não haver repetição de código, como no caso da aula 4.w, onde vendedor e motorista têm as mesmas
+ * informações.
+ * ao digitar 'v.' aqui na Main, consigo chamar os atribuitos de Funcionario para a classe Vendedor (por causa do super)
+ */
--- Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Heranca/src/main/java/Heranca/Vendedor.java
+++ Algoritmos e Lógica/Lógica de Programação e Algoritmos III/Heranca/src/main/java/Heranca/Vendedor.java
@@ -2,48 +2,15 @@ package Heranca;
 
 import java.util.Date;
 
-public class Vendedor {
+public class Vendedor extends Funcionario {
 
-    private String nome;
-    private String cpf;
-    private Date dataNascimento;
-    private float salario;
+    private int totalItensVendidos;
+    private float comissaoPorItem;
 
     //Constructor
     public Vendedor() {
-
-    }
-
-    //Getters and Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public Date getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(Date dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    public float getSalario() {
-        return salario;
-    }
-
-    public void setSalario(float salario) {
-        this.salario = salario;
+        super(); //me dá acesso a todos os atributos de funcionário aqui em vendedor. Sem isso não consigo chamar o obj na Main!
     }
 }
+
+//O vendedor passa a ter todas as informações de Funcionário quando extendo a classe aqui para dentro!
